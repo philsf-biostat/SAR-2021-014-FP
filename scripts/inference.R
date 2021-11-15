ae0741371ec01e1323f21d4e5a0916c18ef0b047
Felipe Figueiredo <philsf79@gmail.com>
2021-11-15 18:40:33 -0300

    obsoletos removidos

--- scripts/inference.R
+++ scripts/inference.R
@@ -6,29 +6,6 @@
 
 # tables ------------------------------------------------------------------
 
-# template p-value table
-# tab_inf <- analytical %>%
-#   # select
-#   select(-id, ) %>%
-#   tbl_summary(
-#     by = group,
-#     include = -c(sexo, idade, imc, dor, tipo, lado),
-#   ) %>%
-#   # include study N
-#   add_overall() %>%
-#   # pretty format categorical variables
-#   bold_labels() %>%
-#   # bring home the bacon!
-#   add_p(
-#     # use Fisher test (defaults to chi-square)
-#     test = all_categorical() ~ "fisher.test",
-#     # use 3 digits in pvalue
-#     pvalue_fun = function(x) style_pvalue(x, digits = 3)
-#   ) %>%
-#   # bold significant p values
-#   bold_p()
-
-# Template Cohen's D table (obs: does NOT compute p)
 tab_inf <- analytical %>%
   mutate(group = relevel(group, "Artrose")) %>%
   # select
